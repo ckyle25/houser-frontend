385244311cdd2629d4b149f4cb4f951eb356ddd1
Kyle Cornwall <kyle.cornwall@healthcatalyst.com>
2017-10-14 11:02:37 -0600

    first commit

---
+++ src/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './reset.css'
+import './Home.css';
+
+class Home extends Component {
+    render() {
+      return (
+        <div>
+            
+        </div>
+      );
+    }
+  }
+  
+  export default Home;
